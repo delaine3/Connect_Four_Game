3ec34214bdb1a5b4e908280c2d218c6646bd8ae2
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-07-10 11:10:22 -0400

    updated score board orientation

--- pages/index.js
+++ pages/index.js
@@ -26,28 +26,51 @@ const Index = ({ player }) => {
             <a>New Game</a>{" "}
           </button>
         </Link>
-
       </h1>
       <h1>Leader Board</h1>
-      <div className="headers">
-      </div>
-     
+      <div className="headers"></div>
 
       {/* Create a row for each player */}
-      <div >
-        <ol >
+      <div>
+        <ul className="score-board">
+        <li className="score" key={player._id}>
+              <span className="slot">
+                <h2>
+                  {player.player_1_nme ? player.player_1_nme : "Player 1"}
+                </h2>{" "}
+              </span>
+             
+              <span className="slot">
+                <h2>
+                  {player.player_2_name ? player.player_2_name : "Player 2"}
+                </h2>{" "}
+              </span>
+              
+              <span className="slot">
+                <h2>Winner : {player.winner}</h2>{" "}
+              </span>
+            </li>
 
-        {player.map((player) => (
-          <li         key={player._id}
-          className="score-inner-grid">
-              <span ><h2>{player.player_1_nme ?player.player_1_nme : "Player 1" }</h2> </span> 
-              <span ><h2> VS</h2> </span> 
-              <span ><h2>{player.player_2_name ? player.player_2_name : "Player 2"}</h2> </span>
-              <span ><h2>Winner : {player.winner}</h2> </span>
+          {player.map((player) => (
+            <li className="score" key={player._id}>
+              <span className="slot">
+                <h2>
+                  {player.player_1_nme ? player.player_1_nme : "Player 1"}
+                </h2>{" "}
+              </span>
+            
+              <span className="slot">
+                <h2>
+                  {player.player_2_name ? player.player_2_name : "Player 2"}
+                </h2>{" "}
+              </span>
 
-          </li>
-        ))}
-        </ol>
+              <span className="slot">
+                <h2> {player.winner}</h2>{" "}
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
@@ -58,7 +81,7 @@ export async function getServerSideProps() {
   await dbConnect();
 
   /* find all the data  our database */
-  const result = await Connect_4_Players.find().sort( { "_id": -1 } )
+  const result = await Connect_4_Players.find().sort({ _id: -1 });
   const player = result.map((doc) => {
     const player = doc.toObject();
     player._id = player._id.toString();
